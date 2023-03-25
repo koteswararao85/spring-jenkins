61841f031e6afb5e620cb4be6655a63f343bed38
koteswararao85 <46752511+koteswararao85@users.noreply.github.com>
2023-03-25 16:30:15 -0500

    Update SpringJenkinsApplication.java

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -16,6 +16,7 @@ public class SpringJenkinsApplication {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("sample"  );
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
